31c1f8f3a67962e7463b0856c60275784f44e114
jasonzeng <zenghongtu@gmail.com>
2023-09-03 21:23:51 +0800

    update

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -29,13 +29,47 @@ def wait_for_service(url):
         time.sleep(0.2)
 
 
-def run_inference(inference_request):
-    '''
-    Run inference on a request.
-    '''
-    response = automatic_session.post(url='http://127.0.0.1:3000/sdapi/v1/txt2img',
-                                      json=inference_request, timeout=600)
+
+def run_inference(params):
+    config = {
+        "baseurl": "http://127.0.0.1:3000",
+        "api": {
+            "txt2img":  ("POST", "/sdapi/v1/txt2img"),
+            "img2img":  ("POST", "/sdapi/v1/img2img"),
+            "getModels": ("GET", "/sdapi/v1/sd-models"),
+            "getOptions": ("GET", "/sdapi/v1/options"),
+            "setOptions": ("POST", "/sdapi/v1/options"),
+        },
+        "timeout": 600
+    }
+
+    api_name = params["api_name"]
+    path = None
+
+    if api_name in config["api"]:
+        api_config = config["api"][api_name]
+    else:
+        raise Exception("Method '%s' not yet implemented")
+
+    api_verb = api_config[0]
+    api_path = api_config[1]
+
+    response = {}
+
+    if api_verb == "GET":
+        response = automatic_session.get(
+                url='%s%s' % (config["baseurl"], api_path),
+                timeout=config["timeout"])
+
+    if api_verb == "POST":
+        response = automatic_session.post(
+                url='%s%s' % (config["baseurl"], api_path),
+                json=params, 
+                timeout=config["timeout"])
+
     return response.json()
+
+
 
 
 # ---------------------------------------------------------------------------- #
